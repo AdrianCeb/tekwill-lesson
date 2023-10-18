b6d7eabe02531a5de74651913b322fdd02ac0c39
Professional3 <adce74@gamail.com>
2023-10-18 20:55:43 +0300

    homework_nr4_1

---
+++ src/main/java/homework_nr4/HomeworkNr4.java
@@ -0,0 +1,27 @@
+package homework_nr4;
+
+public class HomeworkNr4 {
+
+    public static void main(String[] args) {
+
+        int number = 3;
+
+        int result = multiplyByItself(number);
+
+        System.out.println(result);
+        multiply(number);
+
+    }
+     public static int multiplyByItself( int x ){
+
+        return x * x;
+
+     }
+ public static void multiply( int x){
+
+     System.out.println(x * x);
+
+
+ }
+    }
+
